a016ac7b6f0d53d0d97882d52b8b8ce9138ac97c
Mike Goldenberg <csrutil@tuta.io>
2023-08-28 16:29:30 +0200

    changing all APIs and tests

--- models.js
+++ models.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const StockSchema = new mongoose.Schema({
-  symbol: String,
-  price: Number,
-  likes: { type: Number, default: 0 },
-  likedBy: { type: [String], default: [] },
+const StockSchema = new Schema({
+  symbol: { type: String, required: true },
+  likes: { type: [String], default: [] },
 });
+const Stock = mongoose.model("Stock", StockSchema);
 
-module.exports = mongoose.model('Stock', StockSchema);
+exports.Stock = Stock;
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,11 @@
-'use strict';
-
-const StockModel = require('../models');
-const fetch = require('node-fetch');
-const crypto = require('crypto');
-
-// Anonymize the IP address
-function anonymizeIP(ip) {
-// Truncate the IP address to the desired length
-  const truncatedIP = ip.substring(0, ip.lastIndexOf('.')); // Example: "192.168.0"
-
-  // Alternatively, you can hash the IP address
-// const hashedIP = crypto.createHash('sha256').update(ip).digest('hex'); // Example: "a9c824ef3fbbd1a1d26818307c3d91ae1e58855a92492c019a9c9370a55b2c24"
-
-  // Alternatively, you can mask the IP address
-  //const maskedIP = ip.substring(0, ip.lastIndexOf('.')) + '.0'; // Example: "192.168.0.0"
-
-  
- // Return the anonymized IP address
-  return truncatedIP;
-}
+"use strict";
+const StockModel = require("../models").Stock;
+//const fetch = require("node-fetch");
 
 async function createStock(stock, like, ip) {
   const newStock = new StockModel({
     symbol: stock,
-    likes: like ? 1 : 0,
-    likedBy: like ? [ip] : [],
+    likes: like ? [ip] : [],
   });
   const savedNew = await newStock.save();
   return savedNew;
@@ -38,13 +19,12 @@ async function saveStock(stock, like, ip) {
   let saved = {};
   const foundStock = await findStock(stock);
   if (!foundStock) {
-    const createdStock = await createStock(stock, like, ip);
-    saved = createdStock;
+    const createsaved = await createStock(stock, like, ip);
+    saved = createsaved;
     return saved;
   } else {
-    if (like && foundStock.likedBy && foundStock.likedBy.indexOf(ip) === -1) {
-      foundStock.likes += 1;
-      foundStock.likedBy.push(ip);
+    if (like && foundStock.likes.indexOf(ip) === -1) {
+      foundStock.likes.push(ip);
     }
     saved = await foundStock.save();
     return saved;
@@ -52,90 +32,74 @@ async function saveStock(stock, like, ip) {
 }
 
 async function getStock(stock) {
-  try {
-    const response = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
-
-    if (!response.ok) {
-      const errorDetails = await response.text(); // getting more details about the error
-      console.error(`Error details: ${errorDetails}`);
-      throw new Error('Failed to fetch stock data');
-    }
-
-    const { symbol, latestPrice } = await response.json();
-    return { symbol, latestPrice };
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
 }
 
 module.exports = function (app) {
-  app.route('/api/stock-prices').get(async function (req, res) {
-  const { stock, like } = req.query;
- // Anonymize the user's IP address
-  const ip = req.ip; // My `ip` variable is defined
+  //https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/TSLA/quote
 
-  const anonymizedIP = anonymizeIP(ip);
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
+    if (Array.isArray(stock)) {
+      console.log("stocks", stock);
 
-  try {
-    // Normalize stock to be an array
-    const stocks = Array.isArray(stock) ? stock : [stock];
+      const { symbol, latestPrice } = await getStock(stock[0]);
+      const { symbol: symbol2, latestPrice: latestPrice2 } = await getStock(
+        stock[1]
+      );
 
-    if (stocks.length > 2) {
-      throw new Error("Cannot process more than two stocks");
-    }
+      const firststock = await saveStock(stock[0], like, req.ip);
+      const secondstock = await saveStock(stock[1], like, req.ip);
 
-    const stockResponses = [];
-
-    for (let i = 0; i < stocks.length; i++) {
-      const stockData = await getStock(stocks[i]);
-
-      if (!stockData.symbol) {
-        res.json({ stockData: { likes: like ? 1 : 0 } });
-        return;
-      }
-
-      const filter = { symbol: stockData.symbol };
-      let update;
-
-      if (like) {
-        update = {
-          $set: { price: stockData.latestPrice },
-          $addToSet: { likedBy: ip },
-          $inc: { likes: 1 }
-        };
+      let stockData = [];
+      if (!symbol) {
+        stockData.push({
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
       } else {
-        update = {
-          $set: { price: stockData.latestPrice }
-        };
+        stockData.push({
+          stock: symbol,
+          price: latestPrice,
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
       }
 
-      const options = { upsert: true, new: true, setDefaultsOnInsert: true, useFindAndModify: false };
-      let stockDoc = await StockModel.findOneAndUpdate(filter, update, options);
+      if (!symbol2) {
+        stockData.push({
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol2,
+          price: latestPrice2,
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+      }
 
-      stockResponses.push({ stock: stockDoc.symbol, price: stockDoc.price, likes: stockDoc.likes });
+      res.json({
+        stockData,
+      });
+      return;
+    }
+    const { symbol, latestPrice } = await getStock(stock);
+    if (!symbol) {
+      res.json({ stockData: { likes: like ? 1 : 0 } });
+      return;
     }
 
-    // If 2 stocks, calculate rel_likes
-    if (stockResponses.length === 2) {
-      const rel_likes = stockResponses[0].likes - stockResponses[1].likes;
-      stockResponses[0].rel_likes = rel_likes;
-      stockResponses[1].rel_likes = -rel_likes;
+    const oneStockData = await saveStock(symbol, like, req.ip);
+    console.log("One Stock Data", oneStockData);
 
-      delete stockResponses[0].likes;
-      delete stockResponses[1].likes;
-    }
-
-    // Send the response
-  if (stockResponses.length === 1) {
-    res.json({ stockData: stockResponses[0] });
-  } else {
-    res.json({ stockData: stockResponses });
-  }
-
-} catch (error) {
-  console.error(error);
-  res.status(400).json({ error: 'Failed to fetch stock data' });
-}
+    res.json({
+      stockData: {
+        stock: symbol,
+        price: latestPrice,
+        likes: oneStockData.likes.length,
+      },
+    });
   });
 };
